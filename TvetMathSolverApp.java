2eb195d3158158e87af14423b2cfb90b46a4242b
FABRICADO <93220522+FABRICADO08@users.noreply.github.com>
2025-07-15 15:39:22 +0200

    Update TvetMathSolverApp.java

--- TvetMathSolverApp.java
+++ TvetMathSolverApp.java
@@ -1,10 +1,1 @@
-// src/Solver.java
-public interface Solver {
-    /**
-     * This method will contain all the logic for a specific math problem:
-     * 1. Prompt the user for required inputs.
-     * 2. Calculate the solution.
-     * 3. Print the solution with all necessary steps.
-     */
-    void solve();
-}
+
